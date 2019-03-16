6f72c455d4b8d6ecef538130204126e262e31b6a
Casey Kuball <darthfett@gmail.com>
2019-03-16 09:11:11 -0700

    Amethyst Hello World

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use amethyst::prelude::*;
+
+struct Example;
+
+impl EmptyState for Example {
+    fn on_start(&mut self, _:StateData<'_,()>) {
+        println!("Begin!");
+    }
+    fn on_stop(&mut self, _:StateData<'_,()>) {
+        println!("End!");
+    }
+
+    fn update(&mut self, _: StateData<'_, ()>) -> EmptyTrans {
+        println!("Hello from Amethyst!");
+        Trans::Quit
+    }
+}
+
+fn main() {
+    amethyst::start_logger(Default::default());
+    let mut game = Application::new("./", Example, ()).expect("Fatal error");
+    game.run();
+}
