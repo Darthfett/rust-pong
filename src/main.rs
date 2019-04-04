8f4ecb011566e0f2e4968b6c7b73cbabaf67c806
Casey Kuball <darthfett@gmail.com>
2019-04-03 17:57:14 -0700

    WIP sync commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use amethyst::prelude::*;
 
+use amethyst::core::transform::TransformBundle;
 use amethyst::renderer::{
     DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage,
 };
@@ -23,11 +24,13 @@ fn main() -> amethyst::Result<()> {
             .with_pass(DrawFlat2D::new()),
     );
 
+
     let game_data = GameDataBuilder::default()
-    .with_bundle(
-        RenderBundle::new(pipe, Some(config))
+        .with_bundle(
+          RenderBundle::new(pipe, Some(config))
             .with_sprite_sheet_processor()
-    )?;
+        )?
+        .with_bundle(TransformBundle::new())?;
 
     let mut game = Application::new("./", Pong, game_data)?;
 
--- src/pong.rs
+++ src/pong.rs
@@ -1,9 +1,88 @@
 use amethyst::prelude::*;
 
+use amethyst::assets::{AssetStorage, Loader};
+use amethyst::core::transform::Transform;
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+use amethyst::renderer::{
+    Camera, PngFormat, Projection, SpriteRender, SpriteSheet,
+    SpriteSheetFormat, SpriteSheetHandle, Texture, TextureMetadata,
+};
+
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
+const PADDLE_HEIGHT: f32 = 16.0;
+const PADDLE_WIDTH: f32 = 4.0;
+
 pub struct Pong;
+
+#[derive(PartialEq, Eq)]
+pub enum Side {
+    Left,
+    Right,
+}
+
+pub struct Paddle {
+    pub side: Side,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Paddle {
+    fn new(side: Side) -> Paddle {
+        Paddle {
+            side,
+            width: 1.0,
+            height: 1.0,
+        }
+    }
+}
+
+fn initialise_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_z(1.0);
+    world
+        .create_entity()
+        .with(Camera::from(Projection::orthographic(
+            0.0,
+            ARENA_WIDTH,
+            0.0,
+            ARENA_HEIGHT,
+        )))
+        .with(transform)
+        .build();
+}
+
+fn initialise_paddles(world: &mut World) {
+    let mut left_transform = Transform::default();
+    let mut right_transform = Transform::default();
+
+    // Correctly position the paddles.
+    let y = ARENA_HEIGHT / 2.0;
+    left_transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+    right_transform.set_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+
+    // Create a left plank entity.
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Left))
+        .with(left_transform)
+        .build();
+
+    // Create right plank entity.
+    world
+        .create_entity()
+        .with(Paddle::new(Side::Right))
+        .with(right_transform)
+        .build();
+}
 
 impl SimpleState for Pong {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
+
+        initialise_camera(world);
+        initialise_paddles(world);
 
     }
 }
